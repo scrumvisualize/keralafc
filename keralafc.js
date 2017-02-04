bca16dee8db8f9db42d37e0ddc77e791136090bf
unknown <scrumvisualize@gmail.com>
2017-02-04 16:23:33 +1300

    Commit new icon

--- keralafc.js
+++ keralafc.js
@@ -376,7 +376,7 @@ function findMe() {
 		userMarker1 = new google.maps.Marker({	
 		position: {lat: currentLatitude, lng: currentLongitude},
 		zoom: 13,
-		icon:'images/pointer_2.png',
+		icon:'images/pointer2.PNG',
 		title: "Vinod",
 		map: map	
 	});
